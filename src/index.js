c57fb76acc97b0191f4e99978986a5648ae925d6
jedonge0928 <jedonge1995@gmail.com>
2025-09-08 12:40:38 +0900

    4번쨰 커밋

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter as Router} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import { ThemeProvider } from "styled-components";
@@ -16,12 +16,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <BrowserRouter>
       <ScrollTop/>
         <ThemeProvider theme={theme}>
           <App />
         </ThemeProvider>
-      </Router>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
